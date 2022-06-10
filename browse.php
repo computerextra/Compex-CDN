a4964eb4d12d9288ed5478673417445a8f2d5b26
computerextra <81916339+computerextra@users.noreply.github.com>
2022-06-10 16:58:41 +0200

    asd

---
+++ browse.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer Extra CDN</title>
+</head>
+<body>
+<?php
+    include_once("./inc/menu.inc.php");
+?>
+</body>
+</html>
+
---
+++ inc/menu.inc.php
@@ -0,0 +1,14 @@
+<header>
+    <ul>
+        <li>
+            <a href="http://">Start</a>
+        </li>
+        <li>
+            <a href="http://">Browse</a>
+        </li>
+        <form action="search.php" method="post">
+            <input type="text" name="search" id="search" placeholder="Suche" required>
+            <input type="submit" value="Suchen">
+        </form>
+    </ul>
+</header>

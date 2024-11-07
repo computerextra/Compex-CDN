3a9d7b35f1a44b4b3e3414ec86595152be9b7dcc
jayzone91 <register@j4yz0n3.de>
2024-11-07 18:01:11 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h1>Test</h1>
+        <h2>H2</h2>
+        <h3>H3</h3>
+        <h4>H4</h4>
+        <h5>H5</h5>
+        <h6>H6</h6>
+        <p>P</p>
+        <small>small</small>
+      </div>
+      <div>
+        <blockquote>blockquote</blockquote>
+      </div>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+      <div>
+        <code>@radix-ui/react-alert-dialog</code>
+      </div>
+      <Button>Test</Button>
+    </main>
+  );
+}
+
+export default App;
